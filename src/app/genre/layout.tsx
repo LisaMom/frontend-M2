f0e6837bed70f242860ef15114dcb60a49dac2ae
lisa <lisamom1280@gmail.com>
2026-09-15 18:33:12 +0700

    latest of code

---
+++ src/app/genre/layout.tsx
@@ -0,0 +1,12 @@
+
+export default function GenreLayout({
+  children,
+}: {
+  children: React.ReactNode; 
+}) {
+  return (
+    <section className="pt-24 pb-16 min-h-screen">
+      {children}
+    </section>
+  );
+}
